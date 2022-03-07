892704f975dc548e9b9e46f31519993a1a15b9eb
Zeshan Shahid <98495821+ZeshanShahid79@users.noreply.github.com>
2022-03-07 09:36:39 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [activeItem, setActiveItem] = useState([]);
+  useEffect(() => {
+    loadItems();
+    async function loadItems() {
+      try {
+        const response = await fetch(
+          "https://fetch-me.vercel.app/api/shopping/items"
+        );
+        const data = await response.json();
+        setItems(data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }, [items]);
+  function handleOnChange(event) {
+    setSearchValue(event.target.value);
+  }
+  function handleAddItem(title) {
+    setActiveItem(title.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Shopping List:</h2>
+      <ul>
+        {items
+          .filter((item) => {
+            if (searchValue === "") {
+              //  return item.name.en;
+              return activeItem;
+            } else if (
+              item.name.en.toLowerCase().includes(searchValue.toLowerCase())
+            ) {
+              return item.name.en;
+            }
+            return null;
+          })
+          .map(({ name, _id }) => (
+            <button onClick={handleAddItem} key={_id}>
+              {name.en}
+            </button>
+          ))}
+      </ul>
+      <h2>what do you want to buy</h2>
+      <input placeholder="Search..." onChange={handleOnChange}></input>
+    </>
   );
 }
 
---
+++ src/components/addItem.js
@@ -0,0 +1,1 @@
+//export fefault function addItem(){}
